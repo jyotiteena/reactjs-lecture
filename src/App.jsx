09f8236307bfc6c3a6f4f042239b7daa9e619616
jyotiteena <jingarjyoti9413@gmail.com>
2025-08-20 22:11:03 +0530

    variable pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from "react"
 
 const App = () => {
+  var a = 35;
+  var b =10;
+  var sum = a+b;
   return (
    <>
-      <h1>web</h1>
+      <h1>sum of {a}+{b} =  {sum}</h1>
+      {
+        console.log("first")
+      }
+      {console.log("second")}
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe enim ea voluptatibus eius excepturi, tenetur iusto repellendus nostrum, repudiandae deserunt aut, natus nihil? Quis, incidunt voluptates vitae magnam beatae quidem!
       </p>
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <App></App>
+    {/* <App></App> */}
   </StrictMode>,
 )
+
+// StrictMode = only use for developement , remove 2 time output from production mode 
